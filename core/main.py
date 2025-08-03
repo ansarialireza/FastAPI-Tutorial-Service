ad72382e88a0e0f329ffd2e7718a60ad53ed71ba
alireza <58270983+ansarialireza@users.noreply.github.com>
2025-08-03 20:17:51 +0330

    Refactor Expense API endpoints
    - Implemented unique ID generation for expenses
    - Updated response model for POST endpoint to return ExpenseOut
    - Improved error handling for GET, PUT, and DELETE endpoints
    - Cleaned up code for better readability and maintainability

--- core/main.py
+++ core/main.py
@@ -1,58 +1,122 @@
-from fastapi import FastAPI, status, HTTPException
-import uuid
+from fastapi import FastAPI, status, HTTPException, Query
+from pydantic import BaseModel
+from typing import Annotated
+
 
 app = FastAPI()
+
 
 expenses_fake_db = []
 
 
-@app.get("/expenses/", status_code=status.HTTP_200_OK)
+class ExpenseIn(BaseModel):
+    description: str
+    amount: float
+
+
+class ExpenseOut(ExpenseIn):
+    item_id: int
+
+
+def unique_id_generator():
+    max_item_id = 0
+    expenses_fake_db.sort(key=lambda x: x["item_id"])
+    if expenses_fake_db:
+        max_item_id = expenses_fake_db[-1]["item_id"]
+    return max_item_id + 1
+
+
+@app.get(
+    "/expenses/",
+    response_model=list[ExpenseOut],
+    status_code=status.HTTP_200_OK,
+)
 async def get_expenses():
     return expenses_fake_db
 
 
-@app.get("/expenses/{expense_id}", status_code=status.HTTP_200_OK)
+@app.get(
+    "/expenses/{expense_id}",
+    response_model=ExpenseOut,
+    status_code=status.HTTP_200_OK,
+)
 async def get_expense(expense_id: int):
-    for expense in expenses_fake_db:
-        if expense["item_id"] == expense_id:
-            return expense
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Item with id {expense_id} not found")
-
-
-@app.post("/expenses/", status_code=status.HTTP_201_CREATED)
-async def create_expense(description: str, amount: float):
-    item_id = uuid.uuid4()
-    expenses_fake_db.append(
-        {"item_id": item_id, "description": description, "amount": amount}
+    expense = next(
+        (
+            expense
+            for expense in expenses_fake_db
+            if expense["item_id"] == expense_id
+        ),
+        None,
     )
-    return {
-        "message": "Item created successfully",
-        "id": item_id,
-        "description": description,
-        "amount": amount,
-    }
-
-
-@app.put("/expenses/{expense_id}", status_code=status.HTTP_200_OK)
-async def update_expense(expense_id: int, description: str, amount: float):
-    for expense in expenses_fake_db:
-        if expense["item_id"] == expense_id:
-            expense["description"] = description
-            expense["amount"] = amount
+    if expense is not None:
         return expense
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Item with id {expense_id} not found")
+        detail=f"Item with id {expense_id} not found",
+    )
 
 
-@app.delete("/expenses/{expense_id}", status_code=status.HTTP_200_OK)
-async def delete_expense(expense_id: int):
-    for expense in expenses_fake_db:
-        if expense["item_id"] == expense_id:
-            expenses_fake_db.remove(expense)
-            return status.HTTP_204_NO_CONTENT
+@app.post(
+    "/expenses/",
+    response_model=ExpenseOut,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_expense(item: Annotated[ExpenseIn, Query()]):
+    item_id = unique_id_generator()
+    expenses_fake_db.append(
+        {
+            "item_id": item_id,
+            "description": item.description,
+            "amount": item.amount
+        }
+    )
+    return {
+        "message": "Item created successfully",
+        "item_id": item_id,
+        "description": item.description,
+        "amount": item.amount,
+    }
+
+
+@app.put(
+    "/expenses/{expense_id}",
+    response_model=ExpenseOut,
+    status_code=status.HTTP_200_OK,
+)
+async def update_expense(expense_id: int, item: ExpenseIn):
+    expense = next(
+        (
+            expense
+            for expense in expenses_fake_db
+            if expense["item_id"] == expense_id
+        ),
+        None,
+    )
+    if expense is not None:
+        expense["description"] = item.description
+        expense["amount"] = item.amount
+        return expense
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Item with id {expense_id} not found")
+        detail=f"Item with id {expense_id} not found",
+    )
+
+
+@app.delete("/expenses/{expense_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_expense(expense_id: int):
+    expense = next(
+        (
+            expense
+            for expense in expenses_fake_db
+            if expense["item_id"] == expense_id
+        ),
+        None,
+    )
+    if expense is not None:
+        expenses_fake_db.remove(expense)
+        return status.HTTP_204_NO_CONTENT
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Item with id {expense_id} not found",
+    )
